4dec532ae782e221cf3f91d7b64da2198a3ce039
Hendro Steven <hendro.steven@gmail.com>
2018-11-01 17:01:15 +0700

    initial commit

---
+++ src/Log.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Popbox\Loging;
+
+use GuzzleHttp\Client;
+
+class Log{
+
+    private $url;
+    private $toke;
+
+    public function __construct($url, $token){
+        $this->url = $url;
+        $this->token = $token;
+    }
+
+    public function setUrl($url){
+        $this->url = $url;
+    }
+
+    public function getUrl(){
+        return $this->url;
+    }
+
+    public function setToken($token){
+        $this->token = $token;
+    }
+
+    public function getToken(){
+        return $this->token;
+    }
+
+    public function write($logs){
+        if($this->url & $this->token){
+            $client = new Client();
+            $request = new \GuzzleHttp\Psr7\Request('POST', $this->url);
+            $request->setHeader('Content-Type', 'application/json');
+            $request->setHeader('Authorization','Bearer '.$this->token);
+            $request->setBody($logs);
+            $promise = $client->sendAsync($request)->then(function ($response) {
+                //echo 'I completed! ' . $response->getBody();
+            });
+            $promise->wait();
+        }
+    }
+}
